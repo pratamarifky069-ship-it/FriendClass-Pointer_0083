729ab2a3b12e87241c1de9d8eda68b92877f7261
RifkyIkhsan <pratamarifky069-ship-it>
2026-04-28 16:29:06 +0700

    menambahkan penggunaan reference

--- pointerSatu.cpp
+++ pointerSatu.cpp
@@ -13,6 +13,7 @@ int main(){
     mahasiswa mhs{1};   // Object mhs
     mhs.showNim();  // Member Access Operator
 
+    // penggunaan reference untuk mengakses object
     mahasiswa &refMhs = mhs;    // Pointer Reference refMhs
     refMhs.nim = 2; // Member Access Operator
     mhs.showNim();
